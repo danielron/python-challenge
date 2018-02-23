7543653440b05a8fdc55fc35c6c83633ae156c8b
danielron <34010835+danielron@users.noreply.github.com>
2018-02-22 23:08:40 -0500

    Add files via upload

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,41 +1,40 @@
 import csv
 import os
-import datetime
 import usstateabre
 
-# This is the PyBoss option
+# This is pyboss final
 def employees (inputfile, outputfile):
 
 
-    if not os.path.exists(inputfile):
-        print("File not found.")
-        return
-
-    with open(inputfile, 'r') as csvfile:
+    with open(inputfile, newline="", encoding="utf8") as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         firstrow = next(csvreader, None)
 
-
         with open(outputfile, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+            csvwriter = csv.writer(csvfile, delimiter=',')
             csvwriter.writerow(['Emp ID', 'First', 'Last', 'DOB', 'SSN', 'State'])
 
             for row in csvreader:
                 empid = str(row[0])
                 name = str(row[1])
-                DOB = str(row[2])
+                DOB = (row[2])
                 SSN = str(row[3])
                 state = str(row[4])
-                stateabbreviation = usstateabre.us_state_abbrev[state]
 
-                fl = name.split()
-                f = fl[0]
-                l = fl[1]
-     #           DOB = datetime.datetime.strptime(row[2], "%m/%d/%Y").strftime("%d/%m/%Y")
+                stateabbreviation = usstateabre.us_state_abbrev[state]
+                full = name.split()
+                fname = full[0]
+                lname = full[1]
+#                DOB = datetime.datetime.strptime(str(row[2]), "%m/%d/%Y").strftime("%d/%m/%Y")
+                DOB = str(row[2])
+                bdob = DOB.split("-")
+                byr= bdob[0]
+                bmon=bdob[1]
+                bday=bdob[2]
+                dobn = str(bday) + "/" + str(bmon) + "/" + str(byr)
                 finalssn = "*" * (len(SSN) - 6) + SSN[-4:]
 
-                csvwriter.writerow([empid, str(f), str(l),DOB, str(finalssn), str(stateabbreviation)])
+                csvwriter.writerow([empid, str(fname), str(lname), dobn, str(finalssn), str(stateabbreviation)])
 
-#calling function with data as inputs
 employees("employee_data1.csv", "employee_data1out.csv")
 employees("employee_data2.csv", "employee_data2out.csv")
